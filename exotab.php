b1b9d2bd95e2e6dacf9c6cfe79c62f351f5bbfa9
BAMRI321 <bamrialadin8@gmail.com>
2018-10-04 16:06:55 +0200

    test

---
+++ exotab.php
@@ -0,0 +1,23 @@
+<?php
+$tab =array();
+for($i =0; $i<100;$i++)
+{
+    $tab[$i]=rand(0,100);
+}
+
+for($i =0; $i<101;$i++)
+{
+    
+$s=0;
+  for($j=0;$j<100;$j++)
+  {
+      if($tab[$j]==$i)
+       {
+           $s=$s+1;
+       }
+  }
+  echo "la valeur de ".$i." est presente ".$s." fois <br>";
+
+
+}
+?>
